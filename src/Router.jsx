9186ad2570f7ed69709b23a40d68c693a7512bba
한목 <shm3041@naver.com>
2026-06-01 22:47:24 +0900

    refactor: Router path 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Timetable from './pages/Timetable/Timetable'
 import Signin from './pages/Signin/Signin'
@@ -11,9 +11,6 @@ export default function Router() {
       <Route path="/timetable" element={<Timetable />} />
       <Route path="/login" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/Timetable" element={<Navigate to="/timetable" replace />} />
-      <Route path="/Signin" element={<Navigate to="/login" replace />} />
-      <Route path="/Signup" element={<Navigate to="/signup" replace />} />
     </Routes>
   );
 }
